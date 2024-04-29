278f1f924b9e1f206468cf40817d997c37d80a1c
Gabriel Mayer Clary <gabrielm05.clary@gmail.com>
2024-04-29 19:08:56 -0300

    Lista 16 - Questão 2 (em progresso)
    *Estruturação da questão*
    
    Cálculo da função de Ackermann utilizando algoritimos recursivos.

---
+++ Listas 10+/Lista 16/Lista 16.2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void apresentacao();
+int ler_m();
+int ler_n();
+int calc_func(int m, int n);
+void resposta();
+
+
+int main() {
+    apresentacao();
+    int m = ler_m();
+    int n = ler_n();
+    int resultado = calc_func(m, n);
+    resposta(resultado);
+return 0;
+}
+
+void apresentacao(){
+    printf("Calcular imagens da função de Ackerman a partir da leitura das variaveis m e n, por meio de algoritimos recursivos.\n\n");
+}
+
+int ler_m(){
+    int x;
+    printf("Digite aqui o valor de m: ");
+    scanf("%i", &x);
+
+return x;
+}
+
+int ler_n(){
+    int y;
+    printf("Digite aqui o valor de n: ");
+    scanf("%i", &y);
+
+return y;
+}
+
+int calc_func(int m, int n){
+    int a;
+    if(m == 0)
+        a = n + 1;
+return a;
+}
+
+
+void resposta(int resultado){
+    printf("\n\nA partir da função de Ackerman, o resultado é: %d", resultado);
+
+}
